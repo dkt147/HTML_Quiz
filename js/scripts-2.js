c1fb47b10283c6210159b46e827ca088817feebf
Daniyal Khan <dk720820@gmail.com>
2021-12-03 16:49:02 +0500

    Added 10 more questions

--- js/scripts-2.js
+++ js/scripts-2.js
@@ -20,6 +20,55 @@ function showq4(){
 function showq5(){
   $(".question5").fadeIn();
   $(".question4").hide();
+  $(".question6").hide();
+}
+function showq6(){
+  $(".question6").fadeIn();
+  $(".question5").hide();
+}
+function showq7(){
+  $(".question7").fadeIn();
+  $(".question6").hide();
+  $(".question8").hide();
+}
+function showq8(){
+  $(".question8").fadeIn();
+  $(".question7").hide();
+  $(".question9").hide();
+}
+function showq9(){
+  $(".question9").fadeIn();
+  $(".question8").hide();
+  $(".question10").hide();
+}
+function showq10(){
+  $(".question10").fadeIn();
+  $(".question9").hide();
+  $(".question11").hide();
+}
+function showq11(){
+  $(".question11").fadeIn();
+  $(".question10").hide();
+  $(".question12").hide();
+}
+function showq12(){
+  $(".question12").fadeIn();
+  $(".question11").hide();
+  $(".question13").hide();
+}
+function showq13(){
+  $(".question13").fadeIn();
+  $(".question12").hide();
+  $(".question14").hide();
+}
+function showq14(){
+  $(".question14").fadeIn();
+  $(".question13").hide();
+  $(".question15").hide();
+}
+function showq15(){
+  $(".question15").fadeIn();
+  $(".question14").hide();
 }
 function endExam(){
   $(".container").hide();
@@ -28,29 +77,48 @@ function endExam(){
 }
 
 $(document).ready(function(){
+  
   $("#qbtn1next").click(function(event){
     showq2();
-  });
-  $("#qbtn2previous").click(function(event){
-    showq1();
   });
   $("#qbtn2next").click(function(event){
     showq3();
   });
-  $("#qbtn3previous").click(function(event){
-    showq2();
-  });
   $("#qbtn3next").click(function(event){
     showq4();
-  });
-  $("#qbtn4previous").click(function(event){
-    showq3();
   });
   $("#qbtn4next").click(function(event){
     showq5();
   });
-  $("#qbtn5previous").click(function(event){
-    showq4();
+  $("#qbtn5next").click(function(event){
+    showq6();
+  });
+  $("#qbtn6next").click(function(event){
+    showq7();
+  });
+  $("#qbtn7next").click(function(event){
+    showq8();
+  });
+  $("#qbtn8next").click(function(event){
+    showq9();
+  });
+  $("#qbtn9next").click(function(event){
+    showq10();
+  });
+  $("#qbtn10next").click(function(event){
+    showq11();
+  });
+  $("#qbtn11next").click(function(event){
+    showq12();
+  });
+  $("#qbtn12next").click(function(event){
+    showq13();
+  });
+  $("#qbtn13next").click(function(event){
+    showq14();
+  });
+  $("#qbtn14next").click(function(event){
+    showq15();
   });
   $("#submit").click(function(event){
     endExam();
@@ -65,21 +133,67 @@ $(document).ready(function(){
     var button43 = document.getElementById('q43');
     var button52 = document.getElementById('q52');
 
+    var button63 = document.getElementById('q63');
+    var button71 = document.getElementById('q71');
+    var button84 = document.getElementById('q84');
+    var button93 = document.getElementById('q93');
+    var button102 = document.getElementById('q102');
+
+    var button113 = document.getElementById('q113');
+    var button121 = document.getElementById('q121');
+    var button134 = document.getElementById('q134');
+    var button143 = document.getElementById('q143');
+    var button152 = document.getElementById('q152');
+    var button163 = document.getElementById('q163');
+    
+
    if (button13.checked) {
-     result += 20
+     result += 2
    }
    if (button21.checked) {
-     result += 20
+     result += 2
    }
    if (button34.checked) {
-     result += 20
+     result += 2
    }
    if (button43.checked) {
-     result += 20
+     result += 2
    }
    if (button52.checked) {
-     result += 20
-   }
+    result += 2
+  }
+
+  if (button63.checked) {
+    result += 2
+  }
+  if (button71.checked) {
+    result += 2
+  }
+  if (button84.checked) {
+    result += 2
+  }
+  if (button93.checked) {
+    result += 2
+  }
+  if (button102.checked) {
+   result += 2
+ }
+ 
+ if (button113.checked) {
+  result += 2
+}
+if (button121.checked) {
+  result += 2
+}
+if (button134.checked) {
+  result += 2
+}
+if (button143.checked) {
+  result += 2
+}
+if (button152.checked) {
+ result += 2
+}
    else {
      result == 0
    }
